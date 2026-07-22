abb6b30d27839a5fbcc1ed3d2ebae8bf303c8afd
Pritam <ijiopritam@gmail.com>
2026-07-22 10:02:20 +0530

    Locators Commit2

---
+++ src/test/java/locatorpackage/UsingXpathByAxes.java
@@ -0,0 +1,17 @@
+package locatorpackage;
+
+public class UsingXpathByAxes
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
